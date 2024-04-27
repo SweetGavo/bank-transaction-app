876f2324d10d3256fb54ad41564bb579eb63f245
mac <mac@macs-MacBook-Air.local>
2024-04-27 14:45:59 +0100

    port 80

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT
-  await app.listen(port);
-  console.log(`App listening at port ${port}...`);
+  await app.listen(80);
+  console.log(`App listening at port  80...`);
 }
 
 
